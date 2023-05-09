c6a220a198bac45d4a6ebbefca377da66342686e
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-05-09 19:55:11 +0800

    Show a text input when 'not listed' btn is chosen
    The not listed btn is under the pronoun group input.

--- js/another-option.js
+++ js/another-option.js
@@ -1,27 +1,61 @@
 const typeOfWorkRadioBtns = document.querySelectorAll(
-  ".has-another-option > .radio-box > input[type='radio']"
+  ".has-another-option > .radio-box > input[type='radio'][name='work-type']"
+);
+const pronounsRadioBtns = document.querySelectorAll(
+  ".has-another-option > .radio-box > input[type='radio'][name='pronouns']"
 );
 
-typeOfWorkRadioBtns.forEach(btn => {
-  btn.addEventListener("change", () => {
-    let last_btn = typeOfWorkRadioBtns[typeOfWorkRadioBtns.length - 1] 
-    console.log(last_btn.checked);
 
-    if (last_btn.checked) {
-      last_btn.parentElement.nextElementSibling.classList.remove("hide");
-    }
-    else {
-      last_btn.parentElement.nextElementSibling.classList.add("hide");
+const radioBtns = [
+  typeOfWorkRadioBtns,
+  pronounsRadioBtns
+]
 
-    }
 
-    // console.log(btn.parentElement.nextElementSibling);
-    // if (btn.checked) {
+let lastBtn, textInput;
+
+radioBtns.forEach(btns => {
+  
+  btns.forEach(btn => {
+    // console.log(btn, lastBtn, textInput);
+    btn.addEventListener("change", () => {
+      lastBtn = btns[btns.length - 1]
+      textInput = lastBtn.parentElement.nextElementSibling
+
+      if (lastBtn.checked) {
+        textInput.classList.remove("hide");
+      }
+      else {
+        textInput.classList.add("hide");
+
+      }
+    });
+    // Event listener here
+    // if (lastBtn.checked) {
+    //   lastBtn.parentE
     // }
-    // else {
-    //   btn.parentElement.nextElementSibling.classList.add("hide");
-
-    // }
-  });
+  })
 })
 
+// typeOfWorkRadioBtns.forEach(btn => {
+//   btn.addEventListener("change", () => {
+//     let last_btn = typeOfWorkRadioBtns[typeOfWorkRadioBtns.length - 1] 
+//     console.log(last_btn.checked);
+
+//     if (last_btn.checked) {
+//       last_btn.parentElement.nextElementSibling.classList.remove("hide");
+//     }
+//     else {
+//       last_btn.parentElement.nextElementSibling.classList.add("hide");
+
+//     }
+
+//     // console.log(btn.parentElement.nextElementSibling);
+//     // if (btn.checked) {
+//     // }
+//     // else {
+//     //   btn.parentElement.nextElementSibling.classList.add("hide");
+
+//     // }
+//   });
+// })
