2a04b9312a9ce6382063e77060b029948df9251c
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-05-10 17:33:18 +0800

    Add realtime input validation for first name

---
+++ js/input-validation.js
@@ -0,0 +1,22 @@
+const firstName = document.querySelector("#first-name");
+
+console.log(firstName);
+
+firstName.addEventListener("input", () => {
+  let label = firstName.nextElementSibling
+  if (!firstName.value) {
+    firstName.classList.add("error");
+    label.classList.add("error");
+    label.innerHTML = "First Name must not be empty.";
+  }
+  else {
+    firstName.classList.remove("error");
+    label.innerHTML = "First Name";
+    label.classList.remove("error");
+
+  }
+
+});
+
+
+
