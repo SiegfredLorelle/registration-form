5367d28046fd05cd7823c92d2f16285c5c5fb435
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-05-10 17:44:12 +0800

    Add input validation to last name

--- js/input-validation.js
+++ js/input-validation.js
@@ -1,22 +1,30 @@
 const firstName = document.querySelector("#first-name");
+const lastName = document.querySelector("#last-name");
 
-console.log(firstName);
+
+// console.log(firstName);
 
 firstName.addEventListener("input", () => {
-  let label = firstName.nextElementSibling
-  if (!firstName.value) {
-    firstName.classList.add("error");
+  nameCheck(firstName);
+});
+lastName.addEventListener("input", () => {
+  nameCheck(lastName);
+});
+
+
+function nameCheck(name) {
+  let label = name.nextElementSibling
+  if (!name.value) {
+    name.classList.add("error");
     label.classList.add("error");
     label.innerHTML = "First Name must not be empty";
   }
   else {
-    firstName.classList.remove("error");
+    name.classList.remove("error");
     label.innerHTML = "First Name";
     label.classList.remove("error");
-
   }
-
-});
+}
 
 
 
