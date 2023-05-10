dad16ab82dcc05b1d49ab09c5cb62e3b1283a822
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-05-10 19:02:00 +0800

    Add input validation for email

--- js/input-validation.js
+++ js/input-validation.js
@@ -1,16 +1,18 @@
 const firstName = document.querySelector("#first-name");
 const lastName = document.querySelector("#last-name");
 const birthday = document.querySelector("#birthday");
+const email = document.querySelector("#email");
 
 
-firstName.addEventListener("input", () => {
-  nameCheck(firstName);
+
+["focus", "input"].forEach(event => {
+  firstName.addEventListener(event, () => {
+    nameCheck(firstName);
+  });
+  lastName.addEventListener(event, () => {
+    nameCheck(lastName);
+  });
 });
-lastName.addEventListener("input", () => {
-  nameCheck(lastName);
-});
-
-
 
 
 ["focus", "input"].forEach(event => {
@@ -19,17 +21,25 @@ lastName.addEventListener("input", () => {
   });
 });
 
+["focus", "input"].forEach(event => {
+  email.addEventListener(event, () => {
+    emailCheck(email);
+  });
+});
 
 function nameCheck(name) {
+  console.log(name);
+
   let label = name.nextElementSibling
   if (!name.value) {
+    // let msg = label.innerHTML
     name.classList.add("error");
     label.classList.add("error");
-    label.innerHTML = "First Name must not be empty";
+    label.innerHTML = " Must not be empty";
   }
   else {
     name.classList.remove("error");
-    label.innerHTML = "First Name";
+    label.innerHTML = `${name.placeholder}`;
     label.classList.remove("error");
   }
 }
@@ -39,14 +49,27 @@ function birthdayCheck(date) {
   if (!date.value) {
     date.classList.add("error");
     label.classList.add("error");
-    label.innerHTML = "First date must not be empty";
+    label.innerHTML = "Follow mm/dd/yyyy format";
   }
   else {
     date.classList.remove("error");
-    label.innerHTML = "First Name";
+    label.innerHTML = "Date";
     label.classList.remove("error");
   }
 }
 
-
+function emailCheck(email) {
+  let label = email.nextElementSibling
+  let isValid = email.value.includes("@") && email.value.includes(".")
+  if (!isValid) {
+    email.classList.add("error");
+    label.classList.add("error");
+    label.innerHTML = "Follow similar format: siegfredlorelle09@gmail.com";
+  }
+  else {
+    email.classList.remove("error");
+    label.innerHTML = "Email";
+    label.classList.remove("error");
+  }
+}
 
