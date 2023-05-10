6433ef63a75a1cbd6cbf0a135cdaad4aa0e7a024
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-05-11 05:27:11 +0800

    Consider client side validation before result show

--- js/input-validation.js
+++ js/input-validation.js
@@ -14,14 +14,15 @@ const position = document.querySelector("#position");
 const coverLetter = document.querySelector("#cover-letter");
 const resume = document.querySelector("#resume-file");
 
-const workType = document.querySelector("input[type='text'][name='work-type']");
-const pronoun = document.querySelector("input[type='text'][name='pronouns']");
-const disability = document.querySelector("input[type='text'][name='disability']");
-const race = document.querySelector("input[type='text'][name='race']");
+// const workType = document.querySelector("input[type='text'][name='work-type']");
+// const pronoun = document.querySelector("input[type='text'][name='pronouns']");
+// const disability = document.querySelector("input[type='text'][name='disability']");
+// const race = document.querySelector("input[type='text'][name='race']");
 
 const submitBtn = document.querySelector(".control-btns-container > input[type='submit']");
 
 
+let validities = {}
 
 
 
@@ -54,18 +55,6 @@ events.forEach(event => {
   })
   position.addEventListener(event, () => {
     generalCheck(position);
-  })
-  workType.addEventListener(event, () => {
-    generalCheck(workType);
-  })
-  pronoun.addEventListener(event, () => {
-    generalCheck(pronoun);
-  })
-  disability.addEventListener(event, () => {
-    generalCheck(disability);
-  })
-  race.addEventListener(event, () => {
-    generalCheck(race);
   })
 });
 
@@ -114,17 +103,21 @@ events.forEach(event => {
 })
 
 
-function generalCheck(name) {
-  let label = name.nextElementSibling
-  if (!name.value) {
-    name.classList.add("error");
+function generalCheck(input) {
+  let label = input.nextElementSibling
+  if (!input.value) {
+    input.classList.add("error");
     label.classList.add("error");
     label.innerHTML = " Must not be empty";
+    validities[input.name] = false;
+    input.scrollIntoView();
+
   }
   else {
-    name.classList.remove("error");
-    label.innerHTML = `${name.placeholder}`;
+    input.classList.remove("error");
+    label.innerHTML = `${input.placeholder}`;
     label.classList.remove("error");
+    validities[input.name] = true;
   }
 }
 
@@ -142,28 +135,38 @@ function birthdayCheck(date) {
     date.classList.add("error");
     label.classList.add("error");
     label.innerHTML = "Follow mm/dd/yyyy format";
+    validities[date.name] = false;
+    date.scrollIntoView();
+
   }
   else if (givenYear > currentYear) {
     date.classList.add("error");
     label.classList.add("error");
     label.innerHTML = "Invalid year";
+    validities[date.name] = false;
+    date.scrollIntoView();
 
   }
   else if (givenYear > maxValidYear) {
     date.classList.add("error");
     label.classList.add("error");
     label.innerHTML = "Sorry you are too young";
-
+    validities[date.name] = false;
+    date.scrollIntoView();
   }
   else if (givenYear < minValidYear) {
     date.classList.add("error");
     label.classList.add("error");
     label.innerHTML = "Sorry you are too old";
+    validities[date.name] = false;
+    date.scrollIntoView();
   }
   else {
     date.classList.remove("error");
     label.innerHTML = "Date";
     label.classList.remove("error");
+    validities[date.name] = true;
+
   }
 }
 
@@ -174,11 +177,16 @@ function emailCheck(email) {
     email.classList.add("error");
     label.classList.add("error");
     label.innerHTML = "Follow similar format: siegfredlorelle09@gmail.com";
+    validities[email.name] = false;
+    email.scrollIntoView();
+
   }
   else {
     email.classList.remove("error");
     label.innerHTML = "Email";
     label.classList.remove("error");
+    validities[email.name] = true;
+
   }
 }
 
@@ -191,11 +199,16 @@ function contactNumCheck(contactNum) {
     contactNum.classList.add("error");
     label.classList.add("error");
     label.innerHTML = "Follow similar format: +63 945 160 5205";
+    validities[contactNum.name] = false;
+    contactNum.scrollIntoView();
+
   }
   else {
     contactNum.classList.remove("error");
     label.innerHTML = "Mobile Number";
     label.classList.remove("error");
+    validities[contactNum.name] = true;
+
   }
 }
 
@@ -206,16 +219,22 @@ function zipCheck(zipCode) {
     zipCode.classList.add("error");
     label.classList.add("error");
     label.innerHTML = "Must only contain digits";
+    validities[zipCode.name] = false;
+    zipCode.scrollIntoView();
+    
   }
   else if (zipCode.value < 100) {
     zipCode.classList.add("error");
     label.classList.add("error");
     label.innerHTML = "Must be at least 3 digits";
+    validities[zipCode.name] = false;
+    zipCode.scrollIntoView();
   }
   else {
     zipCode.classList.remove("error");
     label.innerHTML = "Postal / Zip Code";
     label.classList.remove("error");
+    validities[zipCode.name] = true;
   }
 }
 
@@ -229,17 +248,22 @@ function coverLetterCheck(coverLetter) {
     coverLetter.classList.add("error");
     label.classList.add("error");
     label.innerHTML = "Must not be empty";
+    validities[coverLetter.name] = false;
+    coverLetter.scrollIntoView();
   }
   else if (numOfChars < 100) {
     coverLetter.classList.add("error");
     label.classList.add("error");
     label.innerHTML = `Must at least be 100 characters \ 
     (currently at ${numOfChars})`;
+    validities[coverLetter.name] = false;
+    coverLetter.scrollIntoView();
   }
   else {
     coverLetter.classList.remove("error");
     label.innerHTML = "Cover Letter";
     label.classList.remove("error");
+    validities[coverLetter.name] = true;
   }
 }
 
@@ -253,12 +277,15 @@ function resumeCheck(resume) {
     container.classList.add("error");
     label.classList.add("error");
     label.innerHTML = "File extension must be .pdf, .txt, .doc, or .docx";
+    validities[resume.name] = false;
+    resume.scrollIntoView();
   }
-
+  
   else {
     container.classList.remove("error");
     label.innerHTML = "Resume";
     label.classList.remove("error");
+    validities[resume.name] = true;
   }
 }
 
@@ -272,10 +299,6 @@ const generalInputs = [
   city, 
   state, 
   position, 
-  workType, 
-  pronoun, 
-  disability, 
-  race
 ]
 submitBtn.addEventListener("click", checkAllInputs);
 
@@ -285,7 +308,7 @@ function checkAllInputs() {
   generalInputs.forEach(input => {
     generalCheck(input);
   });
-
+  
   // Check all stricter inputs
   birthdayCheck(birthday);
   emailCheck(email);
@@ -293,5 +316,63 @@ function checkAllInputs() {
   zipCheck(zipCode);
   coverLetterCheck(coverLetter);
   resumeCheck(resume);
+  
+  let i = 0;
+  for (key of Object.keys(validities)) {
+    if (!validities[key]) {
+      break;
+    }
+    i++;
+  }
 
+  if (i == Object.keys(validities).length) {
+    showResult();
+  }
+  
 }
+
+
+function showResult() {
+  let typeOfWorkVal = document.querySelector("input[name='work-type']:checked");
+  let veteranStatusVal = document.querySelector("input[name='veteran-status']:checked");
+  let pronounsVal = document.querySelector("input[name='pronouns']:checked");
+  let disabilityVal = document.querySelector("input[name='disability']:checked");
+  let raceVal = document.querySelector("input[name='race']:checked");
+  
+
+
+  if (typeOfWorkVal.value == "other") {
+    typeOfWorkVal = typeOfWorkVal.closest("label").nextElementSibling.querySelector("input[type='text']");
+  }
+  if (pronounsVal.value == "other") {
+    pronounsVal = pronounsVal.closest("label").nextElementSibling.querySelector("input[type='text']");
+  }
+  if (disabilityVal.value == "other") {
+    disabilityVal = disabilityVal.closest("label").nextElementSibling.querySelector("input[type='text']");
+  }
+  if (raceVal.value == "other") {
+    raceVal = raceVal.closest("label").nextElementSibling.querySelector("input[type='text']");
+  }
+
+
+  alert(
+    "Form is Valid!\n\n"
+    + `First Name: ${firstName.value}\n`
+    + `Last Name: ${lastName.value}\n`
+    + `Date of Birth: ${birthday.value}\n`
+    + `Email: ${email.value}\n`
+    + `Contact Number: ${contactNum.value}\n\n`
+    + `Street Address: ${street.value}\n`
+    + `Street Address 2: ${street2.value}\n`
+    + `City / Municipality: ${city.value}\n`
+    + `State / Province: ${state.value}\n`
+    + `Postal / Zip Code: ${zipCode.value}\n\n`
+    + `Position: ${position.value}\n`
+    + `Type of Work: ${typeOfWorkVal.value}\n`
+    + `Resume: ${resume.files[0].name}\n`
+    + `Veteran Status: ${veteranStatusVal.value}\n`
+    + `Pronouns: ${pronounsVal.value}\n`
+    + `Disabilities: ${disabilityVal.value}\n`
+    + `Race: ${raceVal.value}\n`
+  );
+};
