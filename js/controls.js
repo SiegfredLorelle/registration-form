480105808b8641b5f58cb2bb2ae9f92c52c30671
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-05-11 01:17:00 +0800

    Clear and go back control buttons work

---
+++ js/controls.js
@@ -0,0 +1,22 @@
+const form = document.querySelector("form");
+const clearBtn = document.querySelector(".control-btns-container > button:nth-child(1)")
+const backTop = document.querySelector(".control-btns-container > button:nth-child(3)")
+
+console.log(clearBtn, backTop, form);
+
+clearBtn.addEventListener("click", () => {
+  if (window.confirm("Are you sure to clear the form?")) {
+    form.reset();
+    goBackTop() ;
+  }
+});
+
+backTop.addEventListener("click", () => {
+  goBackTop();
+});
+
+function goBackTop() {
+  window.location.href = '#title';
+}
+
+
