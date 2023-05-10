f1fcadc0634100fd2ca4b8f04aca9fbbffbaf02d
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-05-10 16:57:21 +0800

    Highlight textfield on text area focus

--- js/textfield-highlight.js
+++ js/textfield-highlight.js
@@ -1,14 +1,20 @@
 const inputs = document.querySelectorAll("input:not([type='submit'])");
-
-
-console.log(inputs);
-
+const textAreas = document.querySelectorAll("textarea");
 
 inputs.forEach(input => {
+  highlightTextField(input);
+});
+
+textAreas.forEach(textArea => {
+  highlightTextField(textArea);
+})
+
+function highlightTextField(input) {
   input.addEventListener("focus", () => {
     input.closest("fieldset").classList.add("selected");
   });
   input.addEventListener("focusout", () => {
     input.closest("fieldset").classList.remove("selected");
   });
-});
+
+}
